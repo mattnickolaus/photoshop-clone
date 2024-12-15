976e4beccb3d0a70bba96879c1a137c0f3b49efd
mattnickolaus <mattnickolaus@gmail.com>
2024-12-15 09:24:40 -0500

    Adjustment panel sliders added to the UI layout, updated ReadMe.md with UI mockup image

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Photoshop Clone
+
+![Mockup Image](Swing-UI-Mockup-Image.png)
+
+The above image shows the basic UI and functionality that will be implemented in this project. 
+
+
+
+
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/w65f898Z)
 # Your Final Project
 This is the base repository for you to start adding code for your final project.
--- src/main/java/photoshopclone/View/AdjustmentsPanel.java
+++ src/main/java/photoshopclone/View/AdjustmentsPanel.java
@@ -2,8 +2,17 @@ package photoshopclone.View;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import java.awt.*;
 
 public class AdjustmentsPanel extends JPanel {
+    private JSlider brightnessSlider;
+    private JSlider contrastSlider;
+    private JSlider saturationSlider;
+    private JSlider vibranceSlider;
+    private JSlider warmthSlider;
+    private JSlider tintSlider;
+    private JSlider sharpnessSlider;
+
     public AdjustmentsPanel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBorder(BorderFactory.createTitledBorder(
@@ -12,7 +21,40 @@ public class AdjustmentsPanel extends JPanel {
                 TitledBorder.CENTER,
                 TitledBorder.TOP
         ));
-        add(new JLabel("Adjustments Placeholder"));
-        // Future: add JSliders for brightness, contrast, etc.
+
+        add(Box.createVerticalStrut(10)); // spacing at top
+
+        // Create and add each slider with a label
+        brightnessSlider = createLabeledSlider("Brightness", -100, 100, 0);
+        contrastSlider = createLabeledSlider("Contrast", -100, 100, 0);
+        saturationSlider = createLabeledSlider("Saturation", -100, 100, 0);
+        vibranceSlider = createLabeledSlider("Vibrance", -100, 100, 0);
+        warmthSlider = createLabeledSlider("Warmth", -100, 100, 0);
+        tintSlider = createLabeledSlider("Tint", -100, 100, 0);
+        sharpnessSlider = createLabeledSlider("Sharpness", 0, 100, 50);
+
+        add(Box.createVerticalGlue()); // push sliders up
+    }
+
+    /**
+     * Helper method to create a label and a slider pair and add them to the panel.
+     */
+    private JSlider createLabeledSlider(String labelText, int min, int max, int value) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+
+        JLabel label = new JLabel(labelText + ":");
+        panel.add(label, BorderLayout.WEST);
+
+        JSlider slider = new JSlider(min, max, value);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(false);
+        slider.setMajorTickSpacing((max - min) / 4);
+        slider.setPreferredSize(new Dimension(150, 40));
+        panel.add(slider, BorderLayout.CENTER);
+
+        add(panel);
+        return slider;
     }
 }
